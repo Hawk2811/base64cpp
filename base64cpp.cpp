8c6b1ea3cdb670a21445964d8bf6c6b7805e2df1
Hawk2811 <2811.pedro@gmail.com>
2025-04-06 00:26:12 -0300

    Add decode function

--- base64cpp.cpp
+++ base64cpp.cpp
@@ -33,5 +33,24 @@ string b64_encode(string input){
 }
 
 string b64_decode(string input){
-
+    string out;
+    vector<int> T(256,-1);
+    for (int i = 0; i < 64; i++){
+        T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[i]] = i;
+    }
+    
+    int val=0, valb=-8;
+    for (char c : input) {
+        if (T[c] == -1){
+            break;
+        }
+        val = (val << 6) + T[c];
+        valb += 6;
+        if (valb >= 0){
+            out.push_back(char((val>>valb)&0xFF));
+            valb -= 8;
+        }
+    }
+    return out;
+    
 }
--- test.cpp
+++ test.cpp
@@ -11,9 +11,10 @@
 using namespace std;
 
 int main(){
-    cout << "Message is 'Hello World'" << endl;
     cout << "Encoded" << endl;
     string output = b64_encode("Hello World!");
+    cout << output << endl;
+    output = b64_decode("SGVsbG8gV29ybGQh");
     cout << output << endl;
     return 0;
 }
